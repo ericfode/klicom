5b906d5926f578952114a97472ab345b379ef05d
Eric Fode <ericfode@gmail.com>
2012-06-11 12:20:25 -0400

    first

---
+++ toplevel.ml
@@ -0,0 +1,25 @@
+let rec main_loop stream =
+	match Stream.peek stream with
+		| None -> ()
+		| Some (Token.Kwd ';') ->
+			Stream.junk stream;
+			main_loop stream
+			
+		| Some token ->
+			begin
+				try match token with
+					| Token.Def ->
+						ignore(Parser.parse_definition stream);
+						print_endline "arsed a function definition.";
+					| Token.Extern ->
+						ignore(Parser.parse_extern stream);
+						print_endline "parsed an extern.";
+					| _ ->
+						ignore(Parser.parse_toplevel stream);
+						print_endline "parsed a top_level expr";
+					with Stream.Error s ->
+						Stream.junk stream;
+						print_endline s;
+					end;
+					print_string "ready> "; flush stdout;
+					main_loop stream	
